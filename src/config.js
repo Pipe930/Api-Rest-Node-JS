e26057d67e0c35719a668b84741c8f9d83b780af
pipe930 <felip.sanchezc@duocuc.cl>
2023-02-14 15:33:30 -0300

    se corrigieron las variables de entorno

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ import { config } from "dotenv";
 
 config();
 
-export const puerto = process.env.PORT_URL;
+export const puerto = process.env.PORT;
 
 export const conexionDB = {
-    DB_USER: process.env.USER || '',
-    DB_PORT: process.env.PORT || 0,
-    DB_DATABASE: process.env.DATABASE || '',
-    DB_PASSWORD: process.env.PASSWORD || '',
-    DB_HOST: process.env.HOST || ''
+    DB_USER: process.env.DB_USER || '',
+    DB_PORT: process.env.DB_PORT || 0,
+    DB_DATABASE: process.env.DB_DATABASE || '',
+    DB_PASSWORD: process.env.DB_PASSWORD || '',
+    DB_HOST: process.env.DB_HOST || ''
 }
